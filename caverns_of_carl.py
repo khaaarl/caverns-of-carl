955ee048d2dfc382bdab532cf5e8f0c5a39258bc
carl <isc09carl@gmail.com>
2024-01-11 18:15:22 -0500

    Light sources in code. Unfortunately, they don't look so good in game with the attached TTS light sources, so I'm removing them in code and just showing the mushrooms or sconces.

--- caverns_of_carl.py
+++ caverns_of_carl.py
@@ -1810,6 +1810,17 @@ class Corridor:
     def walk(self, max_width_iter=None):
         return CorridorWalker(self, max_width_iter=max_width_iter)
 
+    def tile_coords(self, df, include_doors=False):
+        for x, y in self.walk():
+            tile = df.tiles[x][y]
+            if isinstance(tile, DoorTile):
+                if include_doors:
+                    yield (x, y)
+                else:
+                    continue
+            if isinstance(tile, CorridorFloorTile):
+                yield (x, y)
+
     def is_nontrivial(self, df):
         usable_length = 0.0
         for x, y in self.walk():
@@ -1925,6 +1936,60 @@ class Door:
         self.ix = None
 
 
+class LightSource:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+        self.roomix = None
+        self.corridorix = None
+        self.ix = None
+
+    def tts_object(self, df):
+        raise NotImplementedError()
+
+
+class WallSconce(LightSource):
+    def tts_object(self, df):
+        obj = tts_reference_object("Horn Candle Sconce")
+        df.tts_xz(self.x, self.y, obj)
+        obj["Transform"]["rotX"] = 0.0
+        obj["Transform"]["rotY"] = 0.0
+        obj["Transform"]["rotZ"] = 0.0
+        obj["Transform"]["posY"] = 1.8
+        obj["Transform"]["scaleX"] = 0.7
+        obj["Transform"]["scaleY"] = 0.7
+        obj["Transform"]["scaleZ"] = 0.7
+        obj["Locked"] = True
+        # Rotate away from wall
+        posrots = [(0, 1, 90), (1, 0, 180), (0, -1, 270), (-1, 0, 0)]
+        random.shuffle(posrots)
+        for dx, dy, r in posrots:
+            if isinstance(df.tiles[self.x + dx][self.y + dy], WallTile):
+                obj["Transform"]["rotY"] += r
+                obj["Transform"]["posX"] += 0.5 * dx
+                obj["Transform"]["posZ"] += 0.5 * dy
+                break
+        # Currently the attached light ... doesn't look very good.
+        del obj["ChildObjects"]
+        return obj
+
+
+class GlowingMushrooms(LightSource):
+    def tts_object(self, df):
+        obj = tts_reference_object("Blue Mushrooms for Glowing")
+        df.tts_xz(self.x, self.y, obj)
+        obj["Transform"]["rotX"] = 0.0
+        obj["Transform"]["rotY"] = random.randrange(360) * 1.0
+        obj["Transform"]["rotZ"] = 0.0
+        obj["Transform"]["posY"] = 2.0
+        obj["Transform"]["scaleX"] = 0.5
+        obj["Transform"]["scaleY"] = 0.5
+        obj["Transform"]["scaleZ"] = 0.5
+        obj["Locked"] = True
+        # Currently the attached light ... doesn't look very good.
+        del obj["ChildObjects"]
+        return obj
+
+
 class DungeonFloor:
     def __init__(self, config):
         self.config = config
@@ -1939,6 +2004,7 @@ class DungeonFloor:
         self.doors = []
         self.monsters = []
         self.traps = []
+        self.light_sources = []
         self.monster_locations = {}  # (x, y) -> monster
         self.room_neighbors = collections.defaultdict(set)
 
@@ -2206,6 +2272,7 @@ def generate_random_dungeon(config=None):
     place_treasure_in_dungeon(df)
     place_monsters_in_dungeon(df)
     place_traps_in_dungeon(df)
+    place_lights_in_dungeon(df)
     stylize_tiles_in_dungeon(df)
     return df
 
@@ -2857,6 +2924,62 @@ def place_traps_in_dungeon(df):
         df.tiles[x][y].trapixs.add(trap.ix)
 
 
+def place_lights_in_dungeon(df):
+    for room in df.rooms:
+        if isinstance(room, CavernousRoom):
+            cs = list(room.tile_coords())
+            random.shuffle(cs)
+            for x, y in cs[: max(1, int(len(cs) / 20.0))]:
+                light = GlowingMushrooms(x, y)
+                light.roomix = room.ix
+                df.light_sources.append(light)
+        else:
+            cs = []
+            for x, y in room.tile_coords():
+                num_near_walls = 0
+                for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                    tx, ty = x + dx, y + dy
+                    tile = df.tiles[tx][ty]
+                    if isinstance(tile, WallTile):
+                        num_near_walls += 1
+                if num_near_walls != 1:
+                    continue
+                tile = df.tiles[x][y]
+                if isinstance(room, BookshelfTile):
+                    continue
+                cs.append((x, y))
+            random.shuffle(cs)
+            for x, y in cs[: max(1, int(len(cs) / 5.0))]:
+                light = WallSconce(x, y)
+                light.roomix = room.ix
+                df.light_sources.append(light)
+    for corridor in df.corridors:
+        if isinstance(corridor, CavernousCorridor):
+            cs = list(corridor.tile_coords(df, include_doors=False))
+            random.shuffle(cs)
+            for x, y in cs[: max(1, int(len(cs) / 20.0))]:
+                light = GlowingMushrooms(x, y)
+                light.corridorix = corridor.ix
+                df.light_sources.append(light)
+        else:
+            cs = []
+            for x, y in corridor.tile_coords(df, include_doors=False):
+                num_near_walls = 0
+                for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                    tx, ty = x + dx, y + dy
+                    tile = df.tiles[tx][ty]
+                    if isinstance(tile, WallTile):
+                        num_near_walls += 1
+                if num_near_walls >= 1:
+                    cs.append((x, y))
+            random.shuffle(cs)
+            for x, y in cs[: max(1, int(len(cs) / 5.0))]:
+                light = WallSconce(x, y)
+                light.corridorix = corridor.ix
+                df.light_sources.append(light)
+    df.light_sources.append(light)
+
+
 def stylize_tiles_in_dungeon(df):
     for room in df.rooms:
         for x, y in room.tile_coords():
@@ -2934,6 +3057,8 @@ def dungeon_to_tts_blob(df):
                 continue
             df.tts_xz(x, y, obj)
             blob["ObjectStates"].append(obj)
+    for light_source in df.light_sources:
+        blob["ObjectStates"].append(light_source.tts_object(df))
     for monster in df.monsters:
         obj = monster.tts_object(df)
         blob["ObjectStates"].append(obj)
